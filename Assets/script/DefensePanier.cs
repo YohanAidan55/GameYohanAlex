1f092b9c2415607f5d1a8f6fb109bb7f6d7fbf87
YohanAidan55 <45605281+YohanAidan55@users.noreply.github.com>
2022-11-01 18:38:17 +0100

    fix bug panier

--- Assets/script/DefensePanier.cs
+++ Assets/script/DefensePanier.cs
@@ -11,26 +11,26 @@ public class DefensePanier : MonoBehaviour
 
         private void Start()
         {
-            color = this.gameObject.GetComponentInParent<PanContent>().color;
+            color = gameObject.GetComponentInParent<PanContent>().color;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-             if (pointDeVie <= 0)
-             {                       
-                  this.gameObject.GetComponentInParent<PanContent>().isDestroy = true;
-
-                  GameObject.Destroy(this.gameObject.transform.GetChild(0).gameObject);
-
-             }
-        }     
-        
         void OnTriggerEnter2D(Collider2D other)
         {
             if ((other.gameObject.tag == "fantomeMechant") && (color == other.gameObject.GetComponent<IAMechant>().color))
             {
                 nbrEnnemy += 1;
+            }
+        }
+        
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if ((other.gameObject.tag == "fantomeMechant") && (color == other.gameObject.GetComponent<IAMechant>().color))
+            {
+                if (pointDeVie == 0)
+                {                       
+                    gameObject.GetComponentInParent<PanContent>().isDestroy = true;
+                    Destroy(gameObject.transform.GetChild(0).gameObject);
+                }
             }
         }
         
